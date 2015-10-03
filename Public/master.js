9b4a9f1d50cef0e32947b7365718f0afcdf7245a
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-10-02 23:40:53 -0400

    edit master javascript - app and controller

--- Public/master.js
+++ Public/master.js
@@ -1,15 +1,23 @@
-  // sample data we would get back from an api
-  var users = [
-      { 
-        name: 'Chris',
-        email: ''
-      },
-      {
-        name: 'Holly',
-        email: ''
-      }
-  ];  
+angular.module("dynamicFormApp", [])
+.config(['$interpolateProvider', function($interpolateProvider){
+$interpolateProvider.startSymbol('{[{'); 
+$interpolateProvider.endSymbol('}]}'); 
+}
 
-  // assign this data to an object to store all our form data
-  $scope.formData = {};
-  $scope.formData.users = users;
+.controller("mainController", function($scope){
+// sample data we would get back from an api
+var users = [
+    { 
+      name: 'Flower',
+      email: ''
+    },
+    {
+      name: 'Flour',
+      email: ''
+    }
+];  
+
+// assign this data to an object to store all our form data
+$scope.formData = {};
+$scope.formData.users = users;
+}
