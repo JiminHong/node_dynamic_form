cbdc0d1a89c1dc0d95b82a396639b0aa46bfc5ad
JiminHong <jiminhong@JIMINs-MacBook-Pro.local>
2015-10-03 00:53:33 -0400

    comments explanation for app dot js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+//requiring the node packages, ex- name of node package:express
 var express = require('express'),
+// like 'instantiating 'this' package (express)'
 	app = express(),
+	//express handlebars
 	exphbs  = require('express-handlebars');
 
 //
 
 //View Engine
+//ex-helloworld // default template is gonna be 'main'
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+//Turn that engine to view
 app.set('view engine', 'handlebars');
 
+//how you enable a directory to be accessed by users
+//__dirname (in this case : app.js)
 app.use(express.static(__dirname + '/Public'));
 app.use(express.static(__dirname + '/bower_components'));
 
 // Config vars
-//environmental 
+//environmental var
 var port = process.env.PORT || 3000;
 
 // every time user visit here, it goes to / server. 
